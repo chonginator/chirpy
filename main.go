1ec8e6934afe715a196342b33dac7f3fd0729e32
chonginator <benchong1999@yahoo.com.au>
2024-10-24 21:09:32 +1000

    add chirp validation endpoint

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
 	"net/http"
 	"sync/atomic"
@@ -21,6 +22,7 @@ func main() {
 	fileServer := http.FileServer(http.Dir(filepathRoot))
 	mux.Handle("/app/", http.StripPrefix("/app", apiCfg.middlewareMetricsInc(fileServer)))
 	mux.HandleFunc("GET /api/healthz", handlerReadiness)
+	mux.HandleFunc("POST /api/validate_chirp", handlerValidateChirp)
 	mux.HandleFunc("GET /admin/metrics", apiCfg.handlerMetrics)
 	mux.HandleFunc("POST /admin/reset", apiCfg.handlerReset)
 
@@ -31,4 +33,64 @@ func main() {
 
 	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
 	log.Fatal(srv.ListenAndServe())
+}
+
+
+func handlerValidateChirp(w http.ResponseWriter, r *http.Request) {
+	const maxChirpLength = 140
+
+	type parameters struct {
+		Body string `json:"body"`
+	}
+	params := parameters{}
+
+	decoder := json.NewDecoder(r.Body)
+	defer r.Body.Close()
+	err := decoder.Decode(&params)
+	if err != nil {
+		log.Printf("Error decoding parameters: %s", err)
+		w.WriteHeader(500)
+		writeErrorResponse(w)
+		return
+	}
+
+	if params.Body == "" {
+		w.WriteHeader(400)
+		writeErrorResponse(w)
+		return
+	}
+
+	if len(params.Body) > maxChirpLength {
+		log.Printf("Chirp exceeds %d characters", maxChirpLength)
+		w.WriteHeader(400)
+		writeErrorResponse(w)
+		return
+	}
+
+	type successRespBody = struct {
+		Valid bool `json:"valid"`
+	}
+
+	w.WriteHeader(200)
+	w.Header().Set("Content-Type", "application/json")
+	respBody, err := json.Marshal(successRespBody{ Valid: true })
+	if err != nil {
+		log.Printf("Error marshalling JSON: %s", err)
+		return
+	}
+	w.Write(respBody)
+}
+
+func writeErrorResponse(w http.ResponseWriter) {
+	type errRespBody struct {
+		Error string 
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	respBody, err := json.Marshal(errRespBody{ Error: "Something went wrong" })
+	if err != nil {
+		log.Printf("Error marshalling JSON: %s", err)
+		return
+	}
+	w.Write(respBody)
 }
