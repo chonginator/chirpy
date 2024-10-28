86f5dbbf9f4ec02fefa5b750078295f0841ca50d
chonginator <benchong1999@yahoo.com.au>
2024-10-28 13:22:02 +1000

    add get chirps endpoint

---
+++ handler_chirps_get.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"net/http"
+)
+
+func (cfg *apiConfig) handlerChirpsGet(w http.ResponseWriter, r *http.Request) {
+	dbChirps, err := cfg.db.GetChirps(r.Context())
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Error getting dbChirps", err)
+		return
+	}
+
+	chirpsResponse := []Chirp{}
+	for _, chirp := range dbChirps {
+		chirpsResponse = append(chirpsResponse, Chirp{
+			ID: chirp.ID,
+			CreatedAt: chirp.CreatedAt,
+			UpdatedAt: chirp.UpdatedAt,
+			Body: chirp.Body,
+			UserID: chirp.UserID,
+		})
+	}
+	respondWithJSON(w, http.StatusOK, chirpsResponse)
+}
