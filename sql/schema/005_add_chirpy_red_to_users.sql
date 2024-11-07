bbc1d542074c8e289dfc0f86e41e2abc6a091999
chonginator <benchong1999@yahoo.com.au>
2024-11-07 10:27:07 +1000

    change default chirpy red status to false

--- sql/schema/005_add_chirpy_red_to_users.sql
+++ sql/schema/005_add_chirpy_red_to_users.sql
@@ -1,6 +1,6 @@
 -- +goose Up
 ALTER TABLE users
-ADD COLUMN is_chirpy_red BOOLEAN NOT NULL DEFAULT TRUE;
+ADD COLUMN is_chirpy_red BOOLEAN NOT NULL DEFAULT FALSE;
 
 -- +goose Down
 ALTER TABLE users
