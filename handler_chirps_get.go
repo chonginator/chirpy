e9fdbb6a15bb63b0a808010ed7c7b875de6f1d2d
chonginator <benchong1999@yahoo.com.au>
2024-11-08 11:05:01 +1000

    add get chirps by author to get chirps handler

--- handler_chirps_get.go
+++ handler_chirps_get.go
@@ -3,24 +3,44 @@ package main
 import (
 	"net/http"
 
+	"github.com/chonginator/chirpy/internal/database"
 	"github.com/google/uuid"
 )
 
 func (cfg *apiConfig) handlerChirpsRetrieve(w http.ResponseWriter, r *http.Request) {
-	dbChirps, err := cfg.db.GetChirps(r.Context())
-	if err != nil {
-		respondWithError(w, http.StatusInternalServerError, "Error getting dbChirps", err)
-		return
+	var dbChirps []database.Chirp
+
+	authorIDString := r.URL.Query().Get("author_id")
+	if authorIDString == "" {
+		allChirps, err := cfg.db.GetChirps(r.Context())
+		if err != nil {
+			respondWithError(w, http.StatusInternalServerError, "Error getting dbChirps", err)
+			return
+		}
+		dbChirps = allChirps
+	} else {
+		authorID, err := uuid.Parse(authorIDString)
+		if err != nil {
+			respondWithError(w, http.StatusBadRequest, "Invalid user ID", err)
+			return
+		}
+
+		authorChirps, err := cfg.db.GetChirpsByAuthor(r.Context(), authorID)
+		if err != nil {
+			respondWithError(w, http.StatusInternalServerError, "Error getting dbChirps", err)
+			return
+		}
+		dbChirps = authorChirps
 	}
 
 	chirpsResponse := []Chirp{}
 	for _, chirp := range dbChirps {
 		chirpsResponse = append(chirpsResponse, Chirp{
-			ID: chirp.ID,
+			ID:        chirp.ID,
 			CreatedAt: chirp.CreatedAt,
 			UpdatedAt: chirp.UpdatedAt,
-			Body: chirp.Body,
-			UserID: chirp.UserID,
+			Body:      chirp.Body,
+			UserID:    chirp.UserID,
 		})
 	}
 	respondWithJSON(w, http.StatusOK, chirpsResponse)
@@ -41,10 +61,10 @@ func (cfg *apiConfig) handlerChirpsGet(w http.ResponseWriter, r *http.Request) {
 	}
 
 	respondWithJSON(w, http.StatusOK, Chirp{
-		ID: dbChirp.ID,
+		ID:        dbChirp.ID,
 		CreatedAt: dbChirp.CreatedAt,
 		UpdatedAt: dbChirp.UpdatedAt,
-		Body: dbChirp.Body,
-		UserID: dbChirp.UserID,
+		Body:      dbChirp.Body,
+		UserID:    dbChirp.UserID,
 	})
 }
