7893810ba815a6595644a45ae3f0ef02f7d8a3d3
chonginator <benchong1999@yahoo.com.au>
2024-11-08 13:45:40 +1000

    add optional sorting query param to get chirps handler

--- handler_chirps_get.go
+++ handler_chirps_get.go
@@ -2,39 +2,44 @@ package main
 
 import (
 	"net/http"
+	"slices"
 
-	"github.com/chonginator/chirpy/internal/database"
 	"github.com/google/uuid"
 )
 
-func (cfg *apiConfig) handlerChirpsRetrieve(w http.ResponseWriter, r *http.Request) {
-	var dbChirps []database.Chirp
+const (
+	sortAscending  = "asc"
+	sortDescending = "desc"
+)
 
+func (cfg *apiConfig) handlerChirpsRetrieve(w http.ResponseWriter, r *http.Request) {
+	dbChirps, err := cfg.db.GetChirps(r.Context())
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Error getting dbChirps", err)
+		return
+	}
+
+	authorID := uuid.Nil
 	authorIDString := r.URL.Query().Get("author_id")
-	if authorIDString == "" {
-		allChirps, err := cfg.db.GetChirps(r.Context())
-		if err != nil {
-			respondWithError(w, http.StatusInternalServerError, "Error getting dbChirps", err)
-			return
-		}
-		dbChirps = allChirps
-	} else {
-		authorID, err := uuid.Parse(authorIDString)
+	if authorIDString != "" {
+		authorID, err = uuid.Parse(authorIDString)
 		if err != nil {
 			respondWithError(w, http.StatusBadRequest, "Invalid user ID", err)
 			return
 		}
+	}
 
-		authorChirps, err := cfg.db.GetChirpsByAuthor(r.Context(), authorID)
-		if err != nil {
-			respondWithError(w, http.StatusInternalServerError, "Error getting dbChirps", err)
-			return
-		}
-		dbChirps = authorChirps
+	sortBy := r.URL.Query().Get("sort")
+	if sortBy != sortAscending && sortBy != sortDescending {
+		sortBy = sortAscending
 	}
 
 	chirpsResponse := []Chirp{}
 	for _, chirp := range dbChirps {
+		if authorID != uuid.Nil && chirp.UserID != authorID {
+			continue
+		}
+
 		chirpsResponse = append(chirpsResponse, Chirp{
 			ID:        chirp.ID,
 			CreatedAt: chirp.CreatedAt,
@@ -43,6 +48,14 @@ func (cfg *apiConfig) handlerChirpsRetrieve(w http.ResponseWriter, r *http.Reque
 			UserID:    chirp.UserID,
 		})
 	}
+
+	slices.SortFunc(chirpsResponse, func(chirpA, chirpB Chirp) int {
+		if sortBy == sortAscending {
+			return chirpA.CreatedAt.Compare(chirpB.CreatedAt)
+		}
+		return -chirpA.CreatedAt.Compare(chirpB.CreatedAt)
+	})
+
 	respondWithJSON(w, http.StatusOK, chirpsResponse)
 }
 
